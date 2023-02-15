8a149633408887ee5656fbfbd53481eb251b9e81
3532235MikhailMukhin42353 <dzhon.seryy@mail.ru>
2023-02-15 20:47:00 +0300

    sdfdf :wq

---
+++ dz_41/Program.cs
@@ -0,0 +1,39 @@
+﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3*/
+
+Console.WriteLine("Введите кличество элементов в массиве: ");
+int elements = Convert.ToInt32(Console.ReadLine());
+int [] myArray = new int [elements];
+FillArray(myArray);
+PrintArray(myArray);
+Console.WriteLine($"Чисел в массиве, которые больше 0: " + Kolichestvo(myArray));
+
+void FillArray (int [] myArray)
+{
+    for (int i = 0; i < myArray.Length; i++)
+    {
+        Console.Write($"Введите число под индексом {i}: ");
+        myArray[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    Console.WriteLine();
+}
+
+void PrintArray (int [] myArray)
+{
+     for (int i = 0; i < myArray.Length; i++)
+    {
+        Console.Write(myArray[i] + "\t");
+    }
+    Console.WriteLine();
+}
+
+int Kolichestvo (int [] myArray)
+{
+    int count = 0;
+    for (int i = 0; i < myArray.Length; i++)
+    {
+        if (myArray[i] > 0 ) count++;
+    }
+    return count;
+}
